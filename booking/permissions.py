f03af010902e573459ce0fec68f3954181caf21c
Oybek <Oybeklinux@gmail.com>
2025-02-04 13:50:01 +0500

    bookings CRUD finished

--- booking/permissions.py
+++ booking/permissions.py
@@ -1,20 +1,40 @@
-from rest_framework.permissions import BasePermission
+from rest_framework.permissions import BasePermission, SAFE_METHODS
 
-class EditIfOwnerOrAdmin(BasePermission):
+class BookingPermission(BasePermission):
+    """
+    Admin barcha bookinglarni ko‘rishi, yaratishi, o‘zgartirishi va o‘chirishi mumkin.
+    Owner faqat o‘z bookinglarini ko‘rishi va o‘chirishi mumkin.
+    Oddiy foydalanuvchilar hech narsa qila olmaydi.
+    """
 
     def has_permission(self, request, view):
-
-        # if request.user.
-        # if request.method in ['GET', 'DELETE']
-        #     return True  # POST, PUT, DELETE uchun faqat admin yoki owner bo'lishi kerak
-
-        return False
+        """Foydalanuvchi tizimga kirganligini tekshiramiz"""
+        if not request.user or request.user.is_anonymous:
+            return False  # Agar foydalanuvchi anonim bo‘lsa, ruxsat yo‘q
+        print("== 1 ==")
+        if request.user.role == 'admin':
+            return True
+        
+        print("== 2 ==")
+        # POST, PUT, PATCH, DELETE so‘rovlari faqat admin uchun ruxsat etilgan
+        if request.method in ['GET', 'DELETE'] and request.user.role == 'owner':
+            return True
+        print("== 3 ==")
+        return False  # Oddiy userlar hech narsa qila olmaydi
 
     def has_object_permission(self, request, view, obj):
+        """Obyekt bo‘yicha ruxsatlarni tekshiramiz"""
+        if not request.user or request.user.is_anonymous:
+            return False  # Agar foydalanuvchi anonim bo‘lsa, ruxsat yo‘q
+        print("== 4 ==")
         if request.user.role == 'admin':
-            return True  
+            return True  # Admin barcha bookinglarni boshqara oladi
+
+        # owner hammasini ko'ra oladi. Va o'zinikini o'chira oladi
+        if request.user.role == 'owner':
+            if request.method == 'GET':
+                return True
+            elif request.method == 'DELETE' and obj.user == request.user:
+                return True
         
-        if request.user.role == 'owner' and obj.owner == request.user:
-            return request.method in ['GET', 'DELETE']
-            
-        return False
+        return False  # Oddiy userlar hech narsa qila olmaydi
--- booking/views.py
+++ booking/views.py
@@ -1,12 +1,12 @@
 from rest_framework import viewsets, permissions
 from .models import Booking
 from .serializers import BookingSerializer
-from .permissions import EditIfOwnerOrAdmin
+from .permissions import BookingPermission
 
 class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
-    permission_classes = [EditIfOwnerOrAdmin]
+    permission_classes = [BookingPermission]
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
